0b723afe071af341d294c106cf7a90d50fb48bd1
pachedi <dapacheco.87@gmail.com>
2025-07-31 13:02:21 -0300

    cambios_clase_3

--- Clase_3_geo/app_monitoreo_transporte.R
+++ Clase_3_geo/app_monitoreo_transporte.R
@@ -2,40 +2,39 @@
 #x=T
 while(TRUE){
   
-
-url='https://apitransporte.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?'
-
-
-request <- GET(
-  url = url,
-  query = list(client_id = client_id,
-               client_secret = client_secret)
-)
-
-request$status_code
-
-response <- content(request, as = "text", encoding = "UTF-8")
-
-
-df <- fromJSON(response) %>% 
-  data.frame()
-
-cientotreinta <- df %>% 
-  filter(route_short_name =="101A")
-
-bondi <- makeIcon(
-  iconUrl = "https://www.shareicon.net/download/128x128//2015/10/19/658777_transport_512x512.png",
-  iconWidth = 30, iconHeight = 30)
-
-
-mapa <- leaflet(cientotreinta) %>%
-  addTiles() %>%  
-  addMarkers(lng =  ~ longitude,
-             lat =  ~ latitude )#,
-             #icon =bondi)
-
-print(mapa)
-
-
-Sys.sleep(20)
+  url='https://apitransporte.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?'
+  
+  
+  request <- GET(
+    url = url,
+    query = list(client_id = client_id,
+                 client_secret = client_secret)
+  )
+  
+  request$status_code
+  
+  response <- content(request, as = "text", encoding = "UTF-8")
+  
+  
+  df <- fromJSON(response) %>% 
+    data.frame()
+  
+  cientotreinta <- df %>% 
+    filter(route_short_name =="101A")
+  
+  bondi <- makeIcon(
+    iconUrl = "https://www.shareicon.net/download/128x128//2015/10/19/658777_transport_512x512.png",
+    iconWidth = 30, iconHeight = 30)
+  
+  
+  mapa <- leaflet(cientotreinta) %>%
+    addTiles() %>%  
+    addMarkers(lng =  ~ longitude,
+               lat =  ~ latitude )#,
+               #icon =bondi)
+  
+  print(mapa)
+  
+  
+  Sys.sleep(30)
 }

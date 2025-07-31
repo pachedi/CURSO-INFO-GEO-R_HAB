7edba194542cd0e239d884c5d5c2504463fa73e7
pachedi <dapacheco.87@gmail.com>
2025-07-31 10:59:09 -0300

    act

--- Clase_3_geo/app_monitoreo_transporte.R
+++ Clase_3_geo/app_monitoreo_transporte.R
@@ -8,8 +8,8 @@ url='https://apitransporte.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?
 
 request <- GET(
   url = url,
-  query = list(client_id = vclient_id,
-               client_secret = vclient_secret)
+  query = list(client_id = client_id,
+               client_secret = client_secret)
 )
 
 request$status_code
@@ -21,7 +21,7 @@ df <- fromJSON(response) %>%
   data.frame()
 
 cientotreinta <- df %>% 
-  filter(route_short_name =="130A")
+  filter(route_short_name =="101A")
 
 bondi <- makeIcon(
   iconUrl = "https://www.shareicon.net/download/128x128//2015/10/19/658777_transport_512x512.png",
@@ -31,11 +31,11 @@ bondi <- makeIcon(
 mapa <- leaflet(cientotreinta) %>%
   addTiles() %>%  
   addMarkers(lng =  ~ longitude,
-             lat =  ~ latitude,
-             icon =bondi)
+             lat =  ~ latitude )#,
+             #icon =bondi)
 
 print(mapa)
 
 
-Sys.sleep(45)
+Sys.sleep(20)
 }
